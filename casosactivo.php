e35783524eaa3066f0f7bc6e3178c1ec2c791a40
jpsanabria37 <jpsanabria37@misena.edu.co>
2022-03-25 13:22:58 -0600

    Modifiqué algunas estructuras para que manuel empiece con los formularios

--- casosactivo.php
+++ casosactivo.php
@@ -1,22 +1,22 @@
 
 <!-- Page Heading -->
 <h1 class="h3 mb-2 text-gray-800">Casos Activos</h1>
-<p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
+<!--<p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
     For more information about DataTables, please visit the <a target="_blank"
-        href="https://datatables.net">official DataTables documentation</a>.</p>
+        href="https://datatables.net">official DataTables documentation</a>.</p>-->
 
 <!-- DataTales Example -->
 <div class="card shadow mb-4">
     <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+        <button class="btn btn-light btn-block" onclick="abrirUrl('nuevocaso.php', 'renderhtml')"><i class="fas fa-folder-plus"></i> Caso Nuevo</button>
     </div>
     <div class="card-body">
         <div class="table-responsive">
             <table class="table table-bordered" id="dataTableCasoActivo" width="100%" cellspacing="0">
                 <thead>
                     <tr>
-                        <th>Name</th>
-                        <th>Position</th>
+                        <th>Propietario</th>
+                        <th>Placa Vehiculo</th>
                         <th>Office</th>
                         <th>Age</th>
                         <th>Start date</th>
@@ -497,6 +497,28 @@
 </div>
 
 <script type="text/javascript">
+    var table = $('#dataTableCasoActivo').DataTable({
+    language: {
+        "decimal": "",
+        "emptyTable": "No hay información",
+        "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+        "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+        "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+        "infoPostFix": "",
+        "thousands": ",",
+        "lengthMenu": "Mostrar _MENU_ Entradas",
+        "loadingRecords": "Cargando...",
+        "processing": "Procesando...",
+        "search": "Buscar:",
+        "zeroRecords": "Sin resultados encontrados",
+        "paginate": {
+            "first": "Primero",
+            "last": "Ultimo",
+            "next": "Siguiente",
+            "previous": "Anterior"
+        }
+    },
+});
     $(document).ready(function() {
       $('#dataTableCasoActivo').DataTable();
     });
---
+++ nuevocaso.php
@@ -0,0 +1,16 @@
+<h1 class="h3 mb-2 text-gray-800"><i class="fas fa-folder-plus"></i> Caso Nuevo</h1>
+<div class="card">
+	<div class="card-header">
+		<div class="input-group">
+			<input type="text" class="form-control" placeholder="Buscar propietario por su número de cédula...">
+			<div class="input-group-append">
+				<button class="btn btn-secondary" type="button">
+				<i class="fa fa-search"></i>
+				</button>
+			</div>
+		</div>
+	</div>
+	<div class="card-body">
+		
+	</div>
+</div>
